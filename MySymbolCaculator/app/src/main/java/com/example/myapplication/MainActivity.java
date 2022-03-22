e41d81e11af1b6ea2695ba73103212e0e5f8eeac
phamtientung <74658281+phamtientung@users.noreply.github.com>
2022-03-22 14:19:17 +0700

    Add files via upload

--- MySymbolCaculator/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MySymbolCaculator/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,54 +9,60 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edita,editb;
+    EditText edita, editb;
     TextView txtkq;
     Button btcong, bttru, btnhan, btchia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edita= (EditText) findViewById(R.id.eda);
-        editb= (EditText) findViewById(R.id.edb);
-        txtkq= (TextView) findViewById(R.id.txkq);
-        btcong=(Button) findViewById(R.id.btncong);
-        bttru=(Button) findViewById(R.id.btntru);
-        btnhan=(Button) findViewById(R.id.btnnhan);
-        btchia=(Button) findViewById(R.id.btnchia);
+        edita = (EditText) findViewById(R.id.eda);
+        editb = (EditText) findViewById(R.id.edb);
+        txtkq = (TextView) findViewById(R.id.txkq);
+        btcong = (Button) findViewById(R.id.btncong);
+        bttru = (Button) findViewById(R.id.btntru);
+        btnhan = (Button) findViewById(R.id.btnnhan);
+        btchia = (Button) findViewById(R.id.btnchia);
     }
-    public void xulycong(View v){
-        String sa= edita.getText().toString();
-        String sb= edita.getText().toString();
+
+    public void xulycong(View v) {
+        String sa = edita.getText().toString();
+        String sb = editb.getText().toString();
         int a = Integer.parseInt(sa);
-        int b=Integer.parseInt(sb);
-        int tong=a+b;
-        String chuoiTong= String.valueOf(tong);
-        txtkq.setText("tổng là:"+chuoiTong);
+        int b = Integer.parseInt(sb);
+        int tong = a + b;
+        String chuoiTong = String.valueOf(tong);
+        txtkq.setText("tổng là:" + chuoiTong);
     }
-    public void xulytru(View v){
-        String sa= edita.getText().toString();
-        String sb= edita.getText().toString();
+
+    public void xulytru(View v) {
+        String sa = edita.getText().toString();
+        String sb = editb.getText().toString();
         int a = Integer.parseInt(sa);
-        int b=Integer.parseInt(sb);
-        int tru=a-b;
-        String chuoiTru= String.valueOf(tru);
-        txtkq.setText("trừ là:"+chuoiTru);
+        int b = Integer.parseInt(sb);
+        int tru = a - b;
+        String chuoiTru = String.valueOf(tru);
+        txtkq.setText("trừ là:" + chuoiTru);
     }
-    public void xulynhan(View v){
-        String sa= edita.getText().toString();
-        String sb= edita.getText().toString();
+
+    public void xulynhan(View v) {
+        String sa = edita.getText().toString();
+        String sb = editb.getText().toString();
         int a = Integer.parseInt(sa);
-        int b=Integer.parseInt(sb);
-        int nhan=a*b;
-        String chuoiNhan= String.valueOf(nhan);
-        txtkq.setText("nhân là:"+chuoiNhan);
+        int b = Integer.parseInt(sb);
+        int nhan = a * b;
+        String chuoiNhan = String.valueOf(nhan);
+        txtkq.setText("nhân là:" + chuoiNhan);
     }
-    public void xulychia(View v){
-        String sa= edita.getText().toString();
-        String sb= edita.getText().toString();
+
+    public void xulychia(View v) {
+        String sa = edita.getText().toString();
+        String sb = editb.getText().toString();
         int a = Integer.parseInt(sa);
-        int b=Integer.parseInt(sb);
-        int chia=a/b;
-        String chuoiChia= String.valueOf(chia);
-        txtkq.setText("hiệu là:"+chuoiChia);
+        int b = Integer.parseInt(sb);
+        int chia = a / b;
+        String chuoiChia = String.valueOf(chia);
+        txtkq.setText("hiệu là:" + chuoiChia);
     }
+}
