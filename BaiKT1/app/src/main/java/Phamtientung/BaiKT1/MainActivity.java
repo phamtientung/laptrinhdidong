72e46fb874d9d220ff1fdcb92984425fe9a1950b
phamtientung <74658281+phamtientung@users.noreply.github.com>
2022-04-05 14:21:42 +0700

    git

--- BaiKT1/app/src/main/java/Phamtientung/BaiKT1/MainActivity.java
+++ BaiKT1/app/src/main/java/Phamtientung/BaiKT1/MainActivity.java
@@ -2,13 +2,48 @@ package Phamtientung.BaiKT1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edit1, edit2;
+    private Button btn;
+
+    public static final String EDIT1 = "EDIT1";
+    public static final String EDIT2 = "EDIT2";
+
+    public MainActivity()
+    {
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = "maicuongtho";
+                String password = "Cntt60ntu";
+                if(edit1.getText().toString().equals(username) && edit2.getText().toString().equals(password) ){
+                    Toast.makeText(getApplicationContext(),"Đăng nhập thành công",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Tài khoản hoặc mật khẩu sai",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
+
 }
